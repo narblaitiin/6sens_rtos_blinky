81d9736107f9e3cafb1fde3cf317a2fb1c188830
narblaitiin <regis.rousseau@insa-lyon.fr>
2025-09-18 09:24:09 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Code for 6Sens Project : blinky application test for a customed MBDT50Q (based on nRF52840) board
+
+## Overview
+Simply Blinky example to know how to install, to configure and to use Zephyr RTOS.
+The Blinky example shows how to configure GPIO pins as outputs which can also be used to drive LEDs on the hardware usually delivered as "User LEDs" on many of the supported boards in Zephyr. The Blinky sample blinks green LED (Tx LED of RFM95W transmission) and an another green LED (Rx LED of RFM95W transmission) forever using GPIO API.
+
+This application also prints "Hello World" to the console and allows us that the device tree board file is correct (especially UART communication).
+The version of Zephyr RTOS used is the version v3.6.0.
+
+With the jlink runner, you can use either the usual console or the one developed by Segger (JLinkRTTViewer).
+
+For Zephyr documentation, please click on this link: https://docs.zephyrproject.org/3.6.0/
+
+**Board used** : Original MDBT50Q board, powered by battery/solar panel. (see 6sens_prj repository/hardware part, for more information.)
+
+## Building and Running
+The following commands clean build folder, build and flash the sample:
+
+**Command to use**
+````
+west build -t pristine
+
+west build -p always -b mdbt50q_lora_dev applications/6sens_rtos_blinky
+
+west flash --runner jlink
---
+++ src/main.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2025
+ * Regis Rousseau
+ * Univ Lyon, INSA Lyon, Inria, CITI, EA3720
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//  ========== includes ====================================================================
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+
+//  ========== defines =====================================================================
+// time in milliseconds to wait between LED toggles (2000 ms = 2 seconds)
+#define SLEEP_TIME_MS   2000
+
+// device tree node identifiers for the LEDs (transmit and receive indicators)
+#define LED_TX DT_ALIAS(ledtx)
+#define LED_RX DT_ALIAS(ledrx)
+
+//  ========== globals =====================================================================
+// define GPIO configurations for the LEDs
+static const struct gpio_dt_spec led_tx = GPIO_DT_SPEC_GET(LED_TX, gpios);
+static const struct gpio_dt_spec led_rx = GPIO_DT_SPEC_GET(LED_RX, gpios);
+
+//  ========== main ========================================================================
+int8_t main(void)
+{
+	// configure the LEDs as active output pins
+	gpio_pin_configure_dt(&led_tx, GPIO_OUTPUT_ACTIVE);
+	gpio_pin_configure_dt(&led_rx, GPIO_OUTPUT_ACTIVE);
+
+	// turn off both LEDs initially
+	gpio_pin_set_dt(&led_tx, 0);
+	gpio_pin_set_dt(&led_rx, 0);
+
+	// infinite loop to toggle LEDs and print status
+	while (1) {
+		// print "Hello World" message with the board configuration name
+		printk("Hello World! %s\n", CONFIG_BOARD);
+
+		// toggle the transmit LED and check for errors
+		int8_t ret = gpio_pin_toggle_dt(&led_tx);
+		if (ret < 0) {
+			return 0;
+		}
+
+		// toggle the receive LED and check for errors
+		ret = gpio_pin_toggle_dt(&led_rx);
+		if (ret < 0) {
+			return 0;
+		}
+
+		// wait for the defined sleep time before toggling again
+		k_msleep(SLEEP_TIME_MS);
+	}
+	return 0;
+}
