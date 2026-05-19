2870b7009c6033ad5f5e9518bf980ec918d8bf3b
narblaitiin <regis.rousseau@insa-lyon.fr>
2026-05-19 13:18:35 +0200

    update with new zephyr version and new board

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ Simply Blinky example to know how to install, to configure and to use Zephyr RTO
 The Blinky example shows how to configure GPIO pins as outputs which can also be used to drive LEDs on the hardware usually delivered as "User LEDs" on many of the supported boards in Zephyr. The Blinky sample blinks green LEDs forever using GPIO API.
 
 This application also prints "Hello World" to the console and allows us that the device tree board file is correct (especially UART communication).
-The version of Zephyr RTOS used is the version v4.0.0.
+The version of Zephyr RTOS used is the version v4.3.
 
 With the jlink runner, you can use either the usual console or the one developed by Segger (JLinkRTTViewer).
 
-For Zephyr documentation, please click on this link: https://docs.zephyrproject.org/4.0.0/
+For Zephyr documentation, please click on this link: https://docs.zephyrproject.org/4.3.0/
 
 ## Board used
 Original MDBT50Q board, powered by battery/solar panel. (see 6sens_prj repository/hardware part, for more information.)
--- src/main.c
+++ src/main.c
@@ -10,6 +10,8 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/sys/printk.h>
 
 //  ========== defines =====================================================================
 // time in milliseconds to wait between LED toggles (2000 ms = 2 seconds)
@@ -17,12 +19,10 @@
 
 // device tree node identifiers for the LEDs
 #define LED_0 DT_ALIAS(led0)
-#define LED_1 DT_ALIAS(led1)
 
 //  ========== globals =====================================================================
 // define GPIO configurations for the LEDs
 static const struct gpio_dt_spec led_0 = GPIO_DT_SPEC_GET(LED_0, gpios);
-static const struct gpio_dt_spec led_1 = GPIO_DT_SPEC_GET(LED_1, gpios);
 
 //  ========== main ========================================================================
 int8_t main(void)
@@ -30,17 +30,15 @@ int8_t main(void)
 	int8_t ret;
 	
 	// test if GPIO is ready to use
-	if ((!gpio_is_ready_dt(&led_0)) & (!gpio_is_ready_dt(&led_1))) {
+	if (!gpio_is_ready_dt(&led_0)) {
         return 0;
     }
 
 	// configure the LEDs as active output pins
 	gpio_pin_configure_dt(&led_0, GPIO_OUTPUT_ACTIVE);
-	gpio_pin_configure_dt(&led_1, GPIO_OUTPUT_ACTIVE);
 
 	// turn off both LEDs initially
 	gpio_pin_set_dt(&led_0, 0);
-	gpio_pin_set_dt(&led_1, 0);
 
 	// infinite loop to toggle LEDs and print status
 	while (1) {
@@ -52,12 +50,6 @@ int8_t main(void)
 		if (ret < 0) {
 			return 0;
 		}
-
-		ret = gpio_pin_toggle_dt(&led_1);
-		if (ret < 0) {
-			return 0;
-		}
-
 		// wait for the defined sleep time before toggling again
 		k_msleep(SLEEP_TIME_MS);
 	}
